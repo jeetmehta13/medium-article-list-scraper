f2f8dc1ddc18f0cf8395fedce0fbb079a236ae95
jeetsmehta13 <jeetsmehta13@gmail.com>
2020-06-05 02:09:10 +0530

    finalx2

--- routes/hashtag.js
+++ routes/hashtag.js
@@ -1,51 +1,28 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-exp = {}
-
-exp.fetch = async(req, res) => {
+exports.hashtag = async (req, res) => {
     try {
-        request('https://medium.com/iecse-hashtag/latest?limit=50', async(err, response, html) => {
-            if(err)
-                res.sendError(err);
-            if(response.statusCode == 200)
-            {   
+        request('https://medium.com/iecse-hashtag/latest?limit=100', async (err, response, html) => {
+            if (err) res.sendError(err);
+            if (response.statusCode == 200) {
                 const data = [];
                 const $ = cheerio.load(html);
-                $("div.js-block").each(async(i, element) => {
-                    const link = $(element).find("div.postArticle-content").find("a").attr("href")
-                    const author = $(element).find("a.u-accentColor--textDarken").text();
-                    const title = $(element).find("h3.graf--title").text();            
-                    const publishedOn = $(element).find("a.link--darken").find("time").attr("datetime");
-                    const imagelink = ($(element).find("div.postArticle-content").find("img").attr("src")).replace("480", "700");
-                    request(link, async (err, response, html) => {
-                        if (err)
-                            sendError(err);
-                        else if (response.statusCode == 200) {
-                            const $ = cheerio.load(html);
-                            const description = $("div#0337").find("h2").text();
-                            console.log(description);
-                            data.push({
-                                link: link,
-                                author: author,
-                                title: title,
-                                publishedOn: publishedOn,
-                                imagelink: imagelink,
-                                //description: description
-                            })
-                        }
-                    })
+                $("div.js-block").each(async (i, element) => {
+                    data.push({
+                        author: $(element).find("a.u-accentColor--textDarken").text(),
+                        link: $(element).find("div.postArticle-content").find("a").attr("href"),
+                        title: $(element).find("h3.graf--title").text(),
+                        description: $(element).find("h4.graf--subtitle").text(),
+                        publishedOn: $(element).find("a.link--darken").find("time").attr("datetime"),
+                        imagelink: ($(element).find("div.postArticle-content").find("img").attr("src")).replace("480", "700")
+                    });
                 });
-                await res.sendSuccess(data);
+                res.sendSuccess(data);
             }
-            else
-            {
-                res.sendError();
-            }
-        })
-    } catch (error) {
-        res.sendError();
+            else res.sendError();
+        });
+    } catch (err) {
+        return res.sendError(err);
     }
-}
-
-module.exports = exp;
+};
