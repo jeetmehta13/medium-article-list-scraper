52394d1f3643e9147aadc47ff9eb66ef21d1cd0f
jeetsmehta13 <jeetsmehta13@gmail.com>
2020-06-05 02:03:03 +0530

    final

--- routes/hashtag.js
+++ routes/hashtag.js
@@ -13,16 +13,30 @@ exp.fetch = async(req, res) => {
                 const data = [];
                 const $ = cheerio.load(html);
                 $("div.js-block").each(async(i, element) => {
-                    data.push({
-                        author : $(element).find("a.u-accentColor--textDarken").text(),
-                        link: $(element).find("div.postArticle-content").find("a").attr("href"),
-                        title: $(element).find("h3.graf--title").text(),
-                        description: $(element).find("h4.graf--subtitle").text(),
-                        publishedOn: $(element).find("a.link--darken").find("time").attr("datetime"),
-                        imagelink: ($(element).find("div.postArticle-content").find("img").attr("src")).replace("480", "700")
+                    const link = $(element).find("div.postArticle-content").find("a").attr("href")
+                    const author = $(element).find("a.u-accentColor--textDarken").text();
+                    const title = $(element).find("h3.graf--title").text();            
+                    const publishedOn = $(element).find("a.link--darken").find("time").attr("datetime");
+                    const imagelink = ($(element).find("div.postArticle-content").find("img").attr("src")).replace("480", "700");
+                    request(link, async (err, response, html) => {
+                        if (err)
+                            sendError(err);
+                        else if (response.statusCode == 200) {
+                            const $ = cheerio.load(html);
+                            const description = $("div#0337").find("h2").text();
+                            console.log(description);
+                            data.push({
+                                link: link,
+                                author: author,
+                                title: title,
+                                publishedOn: publishedOn,
+                                imagelink: imagelink,
+                                //description: description
+                            })
+                        }
                     })
                 });
-                res.sendSuccess(data);
+                await res.sendSuccess(data);
             }
             else
             {
